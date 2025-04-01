daa2d0c92daf8b28bc14b80d52f78e5e6bd9f1a5
윤준식 <justbeber@naver.com>
2025-04-01 17:48:23 +0900

    Day/2025-04-01 - Laptop

---
+++ VUE_TEST/src/components/MainGallary.vue
@@ -0,0 +1,16 @@
+<script setup>
+import BaseButton from './UI/BaseButton.vue'
+</script>
+<template>
+  <!-- 그리드 컨테이너 -->
+  <main class="component-grid">
+    <!-- 컴포넌트 그룹 (그리드 아이템) -->
+    <section class="component-card">
+      <h2>버튼</h2>
+      <BaseButton variant="primary">Primary</BaseButton>
+      <BaseButton variant="outline">Outline</BaseButton>
+      <BaseButton variant="pill">Pill</BaseButton>
+    </section>
+  </main>
+</template>
+<style scoped></style>
